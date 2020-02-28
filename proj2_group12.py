189aa855fe50fbcc2f143dd1a7b81db5cf9bd7a1
Nicholas Jensen <nickbrujens@gmail.com>
2020-02-28 17:26:52 -0500

    Map array

--- proj2_group12.py
+++ proj2_group12.py
@@ -1,1 +1,19 @@
 import sys
+import cv2
+import numpy as np
+
+
+start_node = [10,20]  # in [x,y]
+goal_node = [30,50]
+radius = 0  # radius of the robot
+clearance = 0  #
+
+
+map = np.zeros([300,200,2])
+obstacle_layer = map[:,:,0]
+map[:, :, 1] = np.inf  # high cost to come
+cost2comelayer = map[:, :, 1]
+cv2.imshow('map',obstacle_layer)
+cv2.waitKey()
+
+
